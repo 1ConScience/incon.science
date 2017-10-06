aa00e9f6a0a57cd7558e0ffc0d7b12e6fef4e774
1ConScience <1conscienceofficiel@gmail.com>
2017-10-07 00:41:47 +0200

    fdpBundle created, for listing all fdp / edit / add

---
+++ src/FdpBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FdpBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('FdpBundle:Default:index.html.twig');
+    }
+}
---
+++ src/FdpBundle/FdpBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FdpBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FdpBundle extends Bundle
+{
+}
